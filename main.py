3af789d610c498869f55d1707b3c88340985f29f
PedroHiro <48255520+PedroHiro@users.noreply.github.com>
2022-01-07 18:39:46 -0300

    ✨ Send messages and visualizations to telegram

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from src.telegram_bot import telegramBot
-from src.driveBot import driveBot
+from src.data.driveBot import driveBot
 
-#bot = telegramBot()
-#bot.start()
+bot = telegramBot()
+bot.start()
 
-driveBot = driveBot()
-print(driveBot.get_data())
